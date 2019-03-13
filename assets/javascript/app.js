38532cd73807f996dd9f948852566e5f97251db6
csvernon <csvernon@ncsu.edu>
2019-03-13 06:33:31 -0400

    game is now slightly working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,13 @@ var i = 1;
 var time = 30;
 var intervalId;
 var correct;
-var ans;
+var ans1;
+var ans2;
+var ans3;
+var ans4;
+var wins = 0;
+var losses= 0;
+var question = "question" + i;
 $("#start").on("click", function(event) {
     question1()
     $("#start").remove();
@@ -12,13 +18,49 @@ function question1() {
     
     $("#top").text("Who is the Mad Titan?");
     $(".ans1").text("⦿ Loki");
+    ans1 = "⦿ Loki";
     $(".ans2").text("⦿ Thanos");
+    ans2 = "⦿ Thanos";
     $(".ans3").text("⦿ Red Skull");
+    ans3 = "⦿ Red Skull";
     $(".ans4").text("⦿ Hela");
+    ans4 = "⦿ Hela";
     correct = "⦿ Thanos";
     i++;
     run();
-        
+    check();
+}
+function question2() {
+    
+    $("#top").text("Where was Thor born?");
+    $(".ans1").text("⦿ Mars");
+    ans1 = "⦿ Mars";
+    $(".ans2").text("⦿ Earth");
+    ans2 = "⦿ Earth";
+    $(".ans3").text("⦿ Jupiter");
+    ans3 = "⦿ Jupiter";
+    $(".ans4").text("⦿ Valhalla");
+    ans4 = "⦿ Valhalla";
+    correct = "⦿ Valhalla";
+    i++;
+    run();
+    check();
+}
+function question3() {
+    
+    $("#top").text("Who is Bruce Banner?");
+    $(".ans1").text("⦿ Ironman");
+    ans1 = "⦿ Ironman";
+    $(".ans2").text("⦿ Thor");
+    ans2 = "⦿ Thor";
+    $(".ans3").text("⦿ The Hulk");
+    ans3 = "⦿ The Hulk";
+    $(".ans4").text("⦿ Captain America");
+    ans4 = "⦿ Captain America";
+    correct = "⦿ The Hulk";
+    i++;
+    run();
+    check();
 }
 function run() {
     clearInterval(intervalId);
@@ -40,9 +82,10 @@ function run() {
 
       //  ...run the stop function.
       stop();
-
+        losses++
       //  Alert the user that time is up.
-      check();
+      $("#top").text("Incorrect, better luck next time");
+      next();
       
     }
   }
@@ -59,3 +102,76 @@ function run() {
     $(".timer").text("Time remaining: " + time + " seconds");
 
   }
+
+  function check() {
+    $(".ans1").on("click", function(event) {
+        stop();
+        
+    if (ans1 == correct){
+        $("#top").text("You are correct");
+        wins++;
+    }
+    else {
+        $("#top").text("Incorrect, better luck next time");
+        losses++;
+    }
+    var windowTimeout = setTimeout(function() {
+        next();
+      }, 5000);
+    });
+    $(".ans2").on("click", function(event) {
+        stop();
+        
+    if (ans2 == correct){
+        $("#top").text("You are correct");
+        wins++;
+    }
+    else {
+        $("#top").text("Incorrect, better luck next time");
+        losses++;
+    }
+    var windowTimeout = setTimeout(function() {
+        next();
+      }, 5000);
+    });
+    $(".ans3").on("click", function(event) {
+        stop();
+        
+    if (ans3 == correct){
+        $("#top").text("You are correct");
+        wins++;
+    }
+    else {
+        $("#top").text("Incorrect, better luck next time");
+        losses++;
+    }
+    var windowTimeout = setTimeout(function() {
+        next();
+      }, 5000);
+    });
+    $(".ans4").on("click", function(event) {
+        stop();
+        
+    if (ans4 == correct){
+        $("#top").text("You are correct");
+        wins++;
+    }
+    else {
+        $("#top").text("Incorrect, better luck next time");
+        losses++;
+    }
+    var windowTimeout = setTimeout(function() {
+        next();
+      }, 5000);
+    });
+  }
+  function next(){
+      if (i === 2) {
+        question2();
+    
+      } else if (i === 3){
+        question3()
+    };
+      }
+    
+  
