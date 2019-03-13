645dab492b2c1ebe29913179199ea51246848584
csvernon <csvernon@ncsu.edu>
2019-03-13 08:13:29 -0400

    this trash is broken as shit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,6 @@ var ans3;
 var ans4;
 var wins = 0;
 var losses= 0;
-var question = "question" + i;
 $("#start").on("click", function(event) {
     question1()
     $("#start").remove();
@@ -18,49 +17,97 @@ function question1() {
     
     $("#top").text("Who is the Mad Titan?");
     $(".ans1").text("⦿ Loki");
-    ans1 = "⦿ Loki";
+    ans1 = "Loki";
     $(".ans2").text("⦿ Thanos");
-    ans2 = "⦿ Thanos";
+    ans2 = "Thanos";
     $(".ans3").text("⦿ Red Skull");
-    ans3 = "⦿ Red Skull";
+    ans3 = "Red Skull";
     $(".ans4").text("⦿ Hela");
-    ans4 = "⦿ Hela";
-    correct = "⦿ Thanos";
-    i++;
+    ans4 = "Hela";
+    correct = "Thanos";
     run();
     check();
+    i = 2;
 }
 function question2() {
     
-    $("#top").text("Where was Thor born?");
-    $(".ans1").text("⦿ Mars");
-    ans1 = "⦿ Mars";
-    $(".ans2").text("⦿ Earth");
-    ans2 = "⦿ Earth";
-    $(".ans3").text("⦿ Jupiter");
-    ans3 = "⦿ Jupiter";
+    $("#top").text("Where was the Soul Stone?");
+    $(".ans1").text("⦿ Knowhere");
+    ans1 = "Knowhere";
+    $(".ans2").text("⦿ Vormir");
+    ans2 = "Vormir";
+    $(".ans3").text("⦿ Nidavellir");
+    ans3 = "Nidavellir";
     $(".ans4").text("⦿ Valhalla");
-    ans4 = "⦿ Valhalla";
-    correct = "⦿ Valhalla";
-    i++;
+    ans4 = "Valhalla";
+    correct = "Vormir";
     run();
     check();
+    i = 3;
 }
 function question3() {
     
-    $("#top").text("Who is Bruce Banner?");
-    $(".ans1").text("⦿ Ironman");
-    ans1 = "⦿ Ironman";
-    $(".ans2").text("⦿ Thor");
-    ans2 = "⦿ Thor";
-    $(".ans3").text("⦿ The Hulk");
-    ans3 = "⦿ The Hulk";
-    $(".ans4").text("⦿ Captain America");
-    ans4 = "⦿ Captain America";
-    correct = "⦿ The Hulk";
-    i++;
+    $("#top").text("Who is the Mad Titan?");
+    $(".ans1").text("⦿ Loki");
+    ans1 = "Loki";
+    $(".ans2").text("⦿ Thanos");
+    ans2 = "Thanos";
+    $(".ans3").text("⦿ Red Skull");
+    ans3 = "Red Skull";
+    $(".ans4").text("⦿ Hela");
+    ans4 = "Hela";
+    correct = "Thanos";
     run();
     check();
+    i = 4;
+}
+function question4() {
+    
+    $("#top").text("What is Agent Coulson's first name?");
+    $(".ans1").text("⦿ Steve");
+    ans1 = "Steve";
+    $(".ans2").text("⦿ John");
+    ans2 = "John";
+    $(".ans3").text("⦿ Cal");
+    ans3 = "Cal";
+    $(".ans4").text("⦿ Phil");
+    ans4 = "Phil";
+    correct = "Phil";
+    run();
+    check();
+    i = 5;
+}
+function question5() {
+
+    $("#top").text("What is Captain Marvel's Name?");
+    $(".ans1").text("⦿ Pepper Potts");
+    ans1 = "Pepper Potts";
+    $(".ans2").text("⦿ Carol Danvers");
+    ans2 = "Carol Danvers";
+    $(".ans3").text("⦿ Jessica Jones");
+    ans3 = "Jessica Jones";
+    $(".ans4").text("⦿ Natalia Romanova");
+    ans4 = "Natalia Romanova";
+    correct = "Carol Danvers";
+    run();
+    check();
+    i = 6;
+}
+function question6() {
+
+    $("#top").text("Who is Bruce Banner?");
+    $(".ans1").text("⦿ Ironman");
+    ans1 = "Ironman";
+    $(".ans2").text("⦿ Thor");
+    ans2 = "Thor";
+    $(".ans3").text("⦿ The Hulk");
+    ans3 = "The Hulk";
+    $(".ans4").text("⦿ Captain America");
+    ans4 = "Captain America";
+    correct = "The Hulk";
+    run();
+    check();
+    i = 7;
 }
 function run() {
     clearInterval(intervalId);
@@ -86,11 +133,10 @@ function run() {
       //  Alert the user that time is up.
       $("#top").text("You're out of time, you'll have to be faster");
       $(".ans1").text("");
-      $(".ans2").html('<img src="https://media1.tenor.com/images/5c604725daf2e673c77cc66bd69b5510/tenor.gif?itemid=5150380" style="height: 200px;right: 16%;"/>');
+      $(".ans2").text("");
       $(".ans3").text("");
       $(".ans4").text("");
-
-
+      $(".giffy").html('<img src="https://media1.tenor.com/images/5c604725daf2e673c77cc66bd69b5510/tenor.gif?itemid=5150380" style="height: 200px;right: 16%;"/>');
 
       var windowTimeout = setTimeout(function() {
         next();
@@ -120,14 +166,18 @@ function run() {
         $("#top").text("You are correct");
         wins++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/3oEdv3Ul8g6BCngQ36/giphy.gif" style="height: 200px;right: 16%;"/>');
 
+        
     }
     else {
         $("#top").text("Incorrect, better luck next time");
         losses++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/6bdgwW6B0ooer3r8AZ/giphy.gif" style="height: 200px;right: 16%;"/>');
     }
     var windowTimeout = setTimeout(function() {
+        blank();
         next();
       }, 5000);
     });
@@ -138,14 +188,17 @@ function run() {
         $("#top").text("You are correct");
         wins++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/3oEdv3Ul8g6BCngQ36/giphy.gif" style="height: 200px;right: 16%;"/>');
+
     }
     else {
         $("#top").text("Incorrect, better luck next time");
         losses++;
         blank();
-
+        $(".giffy").html('<img src="https://media.giphy.com/media/6bdgwW6B0ooer3r8AZ/giphy.gif" style="height: 200px;right: 16%;"/>');
     }
     var windowTimeout = setTimeout(function() {
+        blank();
         next();
       }, 5000);
     });
@@ -156,15 +209,18 @@ function run() {
         $("#top").text("You are correct");
         wins++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/3oEdv3Ul8g6BCngQ36/giphy.gif" style="height: 200px;right: 16%;"/>');
+
 
     }
     else {
         $("#top").text("Incorrect, better luck next time");
         losses++;
         blank();
-
+        $(".giffy").html('<img src="https://media.giphy.com/media/6bdgwW6B0ooer3r8AZ/giphy.gif" style="height: 200px;right: 16%;"/>');
     }
     var windowTimeout = setTimeout(function() {
+        blank();
         next();
       }, 5000);
     });
@@ -175,31 +231,37 @@ function run() {
         $("#top").text("You are correct");
         wins++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/3oEdv3Ul8g6BCngQ36/giphy.gif" style="height: 200px;right: 16%;"/>');
+
 
     }
     else {
         $("#top").text("Incorrect, better luck next time");
         losses++;
         blank();
+        $(".giffy").html('<img src="https://media.giphy.com/media/6bdgwW6B0ooer3r8AZ/giphy.gif" style="height: 200px;right: 16%;"/>');
 
     }
     var windowTimeout = setTimeout(function() {
+        blank();
         next();
       }, 5000);
     });
   }
   function next(){
+    $(".giffy").html("");
       if (i === 2) {
         question2();
-    
       } else if (i === 3){
         question3();
-    }
-       else if (i === 4){
+      } else if (i === 4){
         question4();
-    }
-       else if (i === 5){
+      } else if (i === 5){
         question5();
+      } else if (i === 6){
+        question6();
+      } else if (i === 7){
+        question7();
     }
       }
     
